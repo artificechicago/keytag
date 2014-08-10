2a9972908696c1b06a611fb3da08a71410a0fefa
unknown <crooks@2USLCROOKS.ad.here.com>
2014-08-10 15:16:29 -0500

    Added OpTable

--- queryServer.js
+++ queryServer.js
@@ -6,7 +6,48 @@ var queryServerPort = 8124;
 mongoose.connect('mongodb://localhost/test');
 
 function parseJSONQuery (qStruct) {
-  var q = new mongoose.Query;
+  var q = Object.create(mongoose.Query);
+  q.queryOPTable = {
+    "all" : this.and,
+    "and" : this.any,
+    "box" : this.box,
+    "circle" : this.circle,
+    "comment" : this.comment,
+    "elemMatch" : this.elemMatch,
+    "equals" : this.equals,
+    "exists" : this.exists,
+    "geometry" : this.geometry,
+    "gt" : this.gt,
+    "gte" : this.gte,
+    "hint" : this.hint,
+    "in" : this.in,
+    "intersects" : this.intersects,
+    "lean" : this.lean,
+    "limit" : this.limit,
+    "lt" : this.lt,
+    "lte" : this.lte,
+    "maxDistance" : this.maxDistance,
+    "maxScan" : this.maxScan,
+    "mod" : this.mod,
+    "ne" : this.ne,
+    "near" : this.near,
+    "nearSphere" : this.nearSphere,
+    "nin" : this.nin,
+    "nor" : this.nor,
+    "or" : this.or,
+    "polygon" : this.polygon,
+    "populate" : this.populate,
+    "regex" : this.regex,
+    "select" : this.select,
+    "size" : this.size,
+    "skip" : this.skip,
+    "slice" : this.slice,
+    "snapshot" : this.snapshot, // Double check this
+    "sort" : this.sort,
+    "where" : this.where,
+    "within" : this.within
+  };
+
   var pQS = qStruct;
   do {
     if (pQS.op && pQS.args) {
